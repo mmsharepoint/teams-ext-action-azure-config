b48d48b09b7868e85312fe26778923ef5a5b5b90
Markus Moeller <Markus.Moeller@avanade.com>
2021-05-14 14:41:18 +0200

    Managed Identity fixed

--- src/app/api/Utilities.ts
+++ src/app/api/Utilities.ts
@@ -1,13 +1,24 @@
 import { AppConfigurationClient } from "@azure/app-configuration";
-import { DefaultAzureCredential, DefaultAzureCredentialOptions } from "@azure/identity";
+import { DefaultAzureCredential, EnvironmentCredential, ManagedIdentityCredential } from "@azure/identity";
 import { Config } from "../../model/Config";
 
 export default class Utilities {
   public static async retrieveConfig(): Promise<Config> {
     const connectionString = process.env.AZURE_CONFIG_CONNECTION_STRING!;
-    const credential = new DefaultAzureCredential();
-    console.log(credential);
-    const client = new AppConfigurationClient(connectionString, credential);
+    // const credential = new DefaultAzureCredential();
+    // console.log(credential);
+    // const client = new AppConfigurationClient(connectionString, credential);
+    let client: AppConfigurationClient;
+    if (process.env.AZURE_CLIENT_SECRET) {
+      const credential = new EnvironmentCredential();
+      client = new AppConfigurationClient(connectionString, credential);
+    }
+    else {
+      const credential = new ManagedIdentityCredential();
+      console.log("Managed Identity used");
+      console.log(credential);
+      client = new AppConfigurationClient(connectionString, credential);
+    }
     let siteID = "";
     let listID = "";
     try {
